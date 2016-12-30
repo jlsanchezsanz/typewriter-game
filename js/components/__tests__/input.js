ca7e8d5caf8ec849af0b553632ac5c53ee45292c
Jose Luis Sánchez Sanz <joseluis.sanchez.sanz.contractor@bbva.com>
2016-12-30 17:50:18 +0100

    chore(__tests__/input.js): Wrap input tests in description.

--- js/components/__tests__/input.js
+++ js/components/__tests__/input.js
@@ -5,54 +5,57 @@ var Input = require('../input').default;
 var input;
 var innerInput;
 
-beforeEach(function() {
-  input = TestUtils.renderIntoDocument(<Input/>);
-  innerInput = TestUtils.findRenderedDOMComponentWithTag(input, 'input');
-});
-
-describe('Mount component', function() {
-  it('should have an instance of html input element', function() {
-    expect(innerInput).toExist();
-  });
-});
-
-describe('Check its properties', function() {
-  it('should have "defaultValue" property ', function() {
-    expect(Input.PropTypes.defaultValue).toEqual(React.PropTypes.string);
+describe('Input', function() {
+  beforeEach(function() {
+    input = TestUtils.renderIntoDocument(<Input/>);
+    innerInput = TestUtils.findRenderedDOMComponentWithTag(input, 'input');
   });
 
-  it('should have "autofocus" property', function() {
-    expect(Input.PropTypes.autofocus).toEqual(React.PropTypes.bool);
+  describe('Mount component', function() {
+    it('should have an instance of html input element', function() {
+      expect(innerInput).toExist();
+    });
   });
 
-  it('should have "keyStrokes" property', function() {
-    expect(Input.PropTypes.keyStrokes).toEqual(React.PropTypes.number);    
-  });
-});
+  describe('Check its properties', function() {
+    it('should have "defaultValue" property ', function() {
+      expect(Input.PropTypes.defaultValue).toEqual(React.PropTypes.string);
+    });
 
-describe('Value', function() {
-  it('should update value on input change', function() {
-    innerInput.value = 'randomValue';
-    TestUtils.Simulate.change(innerInput);
-    expect(innerInput.value).toEqual('randomValue');
-  });
-});
+    it('should have "autofocus" property', function() {
+      expect(Input.PropTypes.autofocus).toEqual(React.PropTypes.bool);
+    });
 
-describe('Clean', function() {
-  it('should be cleaned when "clean" method is executed', function() {
-    innerInput.value = 'randomValue';
-    TestUtils.Simulate.change(innerInput);
-    expect(innerInput.value).toEqual('randomValue');
-    input.clean();
-    expect(innerInput.value).toEqual('');
+    it('should have "keyStrokes" property', function() {
+      expect(Input.PropTypes.keyStrokes).toEqual(React.PropTypes.number);
+    });
   });
-});
 
-describe('Publish info', function() {
-  it('should update number of strokes on keydown', function() {
-    expect(input.getKeyStrokes()).toEqual(0);
-    TestUtils.Simulate.keyDown(innerInput, {key: 'a', which: 65});
-    TestUtils.Simulate.change(innerInput);
-    expect(input.getKeyStrokes()).toEqual(1);
+  describe('Value', function() {
+    it('should update value on input keydown', function() {
+      innerInput.value = 'randomValue';
+      TestUtils.Simulate.keyDown(innerInput, {key: 'a', which: 65});
+      TestUtils.Simulate.change(innerInput);
+      expect(innerInput.value).toEqual('randomValue');
+    });
+  });
+
+  describe('Clean', function() {
+    it('should be cleaned when "clean" method is executed', function() {
+      innerInput.value = 'randomValue';
+      TestUtils.Simulate.change(innerInput);
+      expect(innerInput.value).toEqual('randomValue');
+      input.clean();
+      expect(innerInput.value).toEqual('');
+    });
+  });
+
+  describe('Publish info', function() {
+    it('should update number of strokes on keydown', function() {
+      expect(input.getKeyStrokes()).toEqual(0);
+      TestUtils.Simulate.keyDown(innerInput, {key: 'a', which: 65});
+      TestUtils.Simulate.change(innerInput);
+      expect(input.getKeyStrokes()).toEqual(1);
+    });
   });
 });

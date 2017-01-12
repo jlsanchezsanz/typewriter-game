aab9e1f4648e5cdd42045d143fa58436e853fcaf
Jose Luis Sánchez Sanz <jose.sanchez@kairosds.com>
2017-01-12 12:57:34 +0100

    style(typewriter.jsx): change inline jsons.

--- js/components/typewriter.jsx
+++ js/components/typewriter.jsx
@@ -7,7 +7,7 @@ class Typewriter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.',
+      text: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivia un hidalgo de los de lanza en astillero, adarga antigua, rocin flaco y galgo corredor. Una olla de algo mas vaca que carnero, salpicon las mas noches, duelos y quebrantos los sabados, lentejas los viernes, algun palomino de añadidura los domingos, consumian las tres partes de su hacienda. El resto della concluian sayo de velarte, calzas de velludo para las fiestas con sus pantuflos de lo mismo, los dias de entre semana se honraba con su vellori de lo mas fino. Tenia en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que asi ensillaba el rocin como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años, era de complexion recia, seco de carnes, enjuto de rostro; gran madrugador y amigo de la caza. Quieren decir que tenia el sobrenombre de Quijada o Quesada (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por conjeturas verosimiles se deja entender que se llama Quijana; pero esto importa poco a nuestro cuento; basta que en la narracion del no se salga un punto de la verdad.',
       word: '',
       inputDisabled: false,
       time: 30,
@@ -37,14 +37,20 @@ class Typewriter extends Component {
   onTimerDataChange(data) {
     this.setState({ time: data.time });
     if (!data.running) {
-      this.setState({ inputDisabled: true, inputValue: '' });
+      this.setState({
+        inputDisabled: true,
+        inputValue: ''
+      });
     } else {
       this.refs.textPanel.showWord();
     }
   }
 
   onWordNotFound(data) {
-    this.setState({ errors: this.state.errors + 1, inputValue: '' });
+    this.setState({
+      errors: this.state.errors + 1,
+      inputValue: ''
+    });
   }
 
   render() {
@@ -63,7 +69,6 @@ class Typewriter extends Component {
           onWordNotFound={this.onWordNotFound.bind(this)}
           onWordFound={this.onWordFound.bind(this)} />
         <Input
-          ref="input"
           autofocus
           defaultValue={this.state.inputValue}
           disabled={this.state.inputDisabled}

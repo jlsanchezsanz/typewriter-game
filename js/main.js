dae88a035abdfe507eba4b0751edd31de38a9b16
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2017-03-30 08:04:59 +0200

    feat(main.js): add main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-import Hello from './components/hello.jsx';
-import World from './components/world.jsx';
+'use strict';
+
+require('../css/app.css');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Typewriter from './components/typewriter';
+
+if (typeof window !== 'undefined') {
+  window.React = React;
+}
+
+ReactDOM.render(
+  <Typewriter />,
+  document.getElementById('app')
+);
